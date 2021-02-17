04745fbc3fa79145376cffd3039db9a9e38af631
fxia22 <xf1280@gmail.com>
2021-02-16 20:25:05 -0800

    test_locally works

--- test_minival_locally.sh
+++ test_minival_locally.sh
@@ -24,4 +24,7 @@ docker run -v $(pwd)/gibson_challenge_data_2021:/opt/iGibson/gibson2/data \
     --runtime=nvidia \
     ${DOCKER_NAME} \
     /bin/bash -c \
-    "export CONFIG_FILE=/opt/iGibson/gibson2/examples/configs/locobot_social_nav.yaml; export TRACK=social; bash submission.sh"
+    "export CONFIG_FILE=/opt/iGibson/gibson2/examples/configs/locobot_social_nav.yaml; export TASK=social; export SPLIT=minival; export EPISODE_DIR=/opt/iGibson/gibson2/data/episodes_data/social_nav; bash submission.sh"
+    
+# make sure CONFIG_FILE, TASK and EPISODE_DIR are consistent
+# you can use TASK environment variable to switch agent in agent.py
